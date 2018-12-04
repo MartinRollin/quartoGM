ed848f0adcf1954830910c6c6dce33acdc1d98c2
GwenaelleCadic <gcadic@ensc.fr>
2018-12-04 11:04:41 +0100

    exemple tab

--- Quarto/Gwe/Program.cs
+++ Quarto/Gwe/Program.cs
@@ -10,6 +10,21 @@ namespace Gwe
     {
         static void Main(string[] args)
         {
+            char[][][,] tab = new char[4][][,];
+            for (int k = 0; k < 4; k++)
+                tab[k] = new char[4][,];
+            char[,] tab2 = new char[10, 10];
+            tab[0][0] = tab2;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tab[0][0][i, j] = 'a';
+                    Console.Write(tab[0][0][i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
