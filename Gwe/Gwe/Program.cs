2ba0dc1e3b0a21d3da52f554dff504b3df170bc1
Gwenaelle Cadic <gcadic@ensc.fr>
2018-12-05 14:58:43 +0000

    bug qui me saoule

--- Gwe/Gwe/Program.cs
+++ Gwe/Gwe/Program.cs
@@ -33,23 +33,38 @@ namespace Gwe
             for (int l = 0; l < tab[0][0].Length; l++)
                 Console.Write("_");
         }
-    }
+    } 
 
-    //Afficher graphiquement les pièces disponibles ainsi que le numéro à afficher si on veut en prendre une
+    //Afficher graphiquement les pièces disponibles ainsi que le numéro à afficher si on veut en prendre une (lesunes au dessus des autres)
     public static void AfficherPieceDisponible(char[][,] graph, int[] dispo)
     {
-
+        for (int i=0; i<16;i++) //on parcours le tableau nous indiquant les pièces disponibles (de taille 16)
+            if (dispo[i]!=0) // on n'afiche que les pièces disponibles
+            {
+                for (int k = 0; k < graph[0].Length; k++)
+                {
+                    for (int l = 0; l < graph[0].Length; l++)
+                        Console.Write(graph[i][k, l]);
+                    Console.WriteLine();
+                }
+                for (int j = 0; j < (graph[0].Length / 2); j++) //on veut placer le numéro associé à la pièce 
+                    Console.Write(" ");
+                Console.Write(i + 1);
+            }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] tab =
+            char[,] tab1 =
             {
-                {1,2,3},
-                {4,5,6}
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'}
             };
-            Console.Write(tab[1,2]);
+            char[][][,] tab = new char[1][][,];
+            tab[0] = new char[1][,] ;
+            tab[0][0] = tab1;          
             Console.ReadLine();
             ;
 
