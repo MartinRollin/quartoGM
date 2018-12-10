d6bb872aea26606a8eb032130701f5a0c20c3c2f
MartinRollin <rollinmartin.1@gmail.com>
2018-12-10 11:36:10 +0100

    maintenant ca marche
    generation tableau graphique des pieces

--- Quarto/Quarto/Program.cs
+++ Quarto/Quarto/Program.cs
@@ -8,26 +8,56 @@ namespace Quarto
 {
     class Program
     {
-        //public static void AfficherVide(int[] tab)
+        public static string[] GenererPiece(string strPiece)
+        {
 
-        //public static void Afficher
+            string[] tabPiece = new string[10];
+
+            for (int i = 0; i < 8; i++)
+            {
+                tabPiece[i] = strPiece.Substring(i * 13, 13);
+            }
+
+            return tabPiece;
+        }
+
+        public static string[][] CreerTableauPieceGraphique()  //renvoie un tableau qui comprend le graphisme de chaque piece par ligne en chaine de caractere. la piece vide est la piece 17
+        {
+            string[] tab = new string[8];
+            string[][] tableauPieceGraphique = new string[16][];
+            // les chaines de caracteres suivantes correspondent chacune à une piece. Le premier caractere de la ligne (ici "b") code la couleur de la piece
+            tab[0] = "b            b   ▄▀▀▀▀▄   b  █      █  b  █▀▄▄▄▄▀█  b  █      █  b   ▀▄▄▄▄▀   b            b            ";
+            tab[1] = "b            b   ▄▀▀▀▀▄   b  █      █  b  █▀▄▄▄▄▀█  b  █      █  b  █      █  b   ▀▄▄▄▄▀   b            ";
+            tab[2] = "b            b   ▄████▄   b  ████████  b  █▀████▀█  b  █      █  b   ▀▄▄▄▄▀   b            b            ";
+            tab[3] = "b            b   ▄████▄   b  ████████  b  █▀████▀█  b  █      █  b  █      █  b   ▀▄▄▄▄▀   b            ";
+            tab[4] = "b            b    ▄▀▀▄    b  ▄▀    █▄  b  █▀▄ ▄▀ █  b  █  █  ▄▀  b   ▀▄█▄▀    b            b            ";
+            tab[5] = "b            b    ▄▀▀▄    b  ▄▀    █▄  b  █▀▄ ▄▀ █  b  █  █   █  b  █  █  ▄▀  b   ▀▄█▄▀    b            ";
+            tab[6] = "b            b    ▄██▄    b  ▄██████▄  b  █▀███▀ █  b  █  █  ▄▀  b   ▀▄█▄▀    b            b            ";
+            tab[7] = "b            b    ▄██▄    b  ▄██████▄  b  █▀███▀ █  b  █  █   █  b  █  █  ▄▀  b   ▀▄█▄▀    b            ";
+
+
+            //generation des pieces blanches
+            for (int i = 0; i < 8; i++)
+            {
+                tableauPieceGraphique[i] = GenererPiece(tab[i]);
+            }
+
+
+            // generation des pieces noires
+            for (int i = 0; i < 8; i++)
+            {
+                tableauPieceGraphique[7 + i] = new string[8];
+                for (int j = 0; i < 8; j++)
+                {
+                    tableauPieceGraphique[7 + i][j] = "v" + tableauPieceGraphique[i][j].Substring(1, 12);
+                }
+
+            }
+            return tableauPieceGraphique;
+        }
+
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Euh.... Does it work?");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("What about now?");
-            Console.ReadLine();
-            Console.WriteLine("vive les chameaux chalumeaux");
-
-            char[,][] tab = new char [2,1] [];
-            tab[0, 0] = new char [] { 'l', 'a' };
-            tab[1, 0] = new char[] { 'r', 'e' };
-            foreach (char [] e in tab) 
-                foreach (char j in e)
-                {
-                    Console.Write(j);
-                }
 
         }
     }
